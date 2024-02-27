118ef6b71d63799df8d9fa9ab6311a22cc2c8dea
Lynn Musabayane <musabayanelynn2@gmail.com>
2024-02-27 16:00:16 +0200

    CRUD partial

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,36 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to root_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params) # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to root_path
+  end
+
+  def destroy
+    @tasks = Task.find(params[:id])
+    @task.destroy
+    # No need for app/views/restaurants/destroy.html.erb
+    redirect_to root_path, status: :see_other
+  end
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
